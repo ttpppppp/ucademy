c83f3d83575578eb550b3c530307e7e63ef8d2c7
ttpppppp <despacitovv@gmail.com>
2024-07-17 17:39:53 +0700

    first commit

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,41 +1,59 @@
+import createUser from "@/lib/actions/user.actions";
 import { WebhookEvent } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 import { Webhook } from "svix";
 
-export async function POST(req: Request) {
+export async function POST(req: Request): Promise<Response> {
   const svix_id = req.headers.get("svix-id") ?? "";
   const svix_timestamp = req.headers.get("svix-timestamp") ?? "";
   const svix_signature = req.headers.get("svix-signature") ?? "";
 
-  if(!process.env.WEBHOOK_SECRET){
-    throw new Error("WEBHOOK_SECRET is not set");
+  if (!process.env.WEBHOOK_SECRET) {
+    console.error("WEBHOOK_SECRET is not set");
+    return new Response("Internal Server Error", { status: 500 });
   }
-  
-  const payload = await req.json();
-  const body = JSON.stringify(payload);
 
-  const sivx = new Webhook(process.env.WEBHOOK_SECRET);
-
-  let msg : WebhookEvent;
-  
+  let payload;
   try {
-    msg = sivx.verify(body, {
+    payload = await req.json();
+  } catch (err) {
+    console.error("Failed to parse JSON payload:", err);
+    return new Response("Bad Request", { status: 400 });
+  }
+
+  const body = JSON.stringify(payload);
+  const svix = new Webhook(process.env.WEBHOOK_SECRET);
+  let msg: WebhookEvent;
+
+  try {
+    msg = svix.verify(body, {
       "svix-id": svix_id,
       "svix-timestamp": svix_timestamp,
       "svix-signature": svix_signature,
     }) as WebhookEvent;
   } catch (err) {
+    console.error("Webhook verification failed:", err);
     return new Response("Bad Request", { status: 400 });
   }
 
   const eventType = msg.type;
+  console.log("Received event type:", eventType);
 
-  if(eventType === "user.created"){
-    console.log(msg.data);
-    
+  if (eventType === "user.created") {
+    const { id, username, email_addresses , image_url} = msg.data;
+
+    const user =  await createUser({
+      clerkId : id,
+      username : username!,
+      email : email_addresses[0].email_address,
+      avatar : image_url
+    });
+
+    return NextResponse.json({
+      message : "OK",
+      user : user
+    })
   }
-  console.log(msg);
-
-  // Rest
 
   return new Response("OK", { status: 200 });
 }
