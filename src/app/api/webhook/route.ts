99feb0e6be306e5a574655565cbdc53db4df99d7
ttpppppp <despacitovv@gmail.com>
2024-07-17 12:57:25 +0700

    first commit

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,16 +1,19 @@
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { Webhook } from "svix";
 
-const webhookSecret: string = process.env.WEBHOOK_SECRET || "";
-
 export async function POST(req: Request) {
   const svix_id = req.headers.get("svix-id") ?? "";
   const svix_timestamp = req.headers.get("svix-timestamp") ?? "";
   const svix_signature = req.headers.get("svix-signature") ?? "";
 
+  if(!process.env.WEBHOOK_SECRET){
+    throw new Error("WEBHOOK_SECRET is not set");
+  }
+  
   const payload = await req.json();
   const body = JSON.stringify(payload);
-  const sivx = new Webhook(webhookSecret);
+
+  const sivx = new Webhook(process.env.WEBHOOK_SECRET);
 
   let msg : WebhookEvent;
   
